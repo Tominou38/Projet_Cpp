e5422cada1e49d42a197b35918b5171e17520ba1
Tom Jullien <jullien@insa-toulouse.fr>
2022-04-12 16:54:08 +0200

    capteur v3

--- Capteur.cpp/Capteur.cpp.ino
+++ Capteur.cpp/Capteur.cpp.ino
@@ -2,7 +2,34 @@
 #include <Adafruit_BMP085.h>
 using namespace std; 
 Adafruit_BMP085 bmp;
+#include <Wire.h>
+#include "rgb_lcd.h"
+#include <Servo.h>
 
+// Create a new servo object:
+Servo myservo;
+Servo myservo2;
+// Define the servo pin:
+#define servoPin 2
+#define servoPin2 3
+// Create a variable to store the servo position:
+int angle = 0;
+
+#include "DHT.h"
+#define DHTPIN 6
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+rgb_lcd lcd;
+const int colorR = 135;
+const int colorG = 143;
+const int colorB = 255;
+
+   //-------------------------------
+   // DES CLASSES CAPTEUR 
+   //-------------------------------
 
 class capteur {
   protected : 
@@ -42,15 +69,31 @@ class BHP: public capteur {
     }
 };
 
-class DHT : public capteur{
+class DHTCapt : public capteur{
   protected : 
     float Temp;
     float Hum;
   public :
-    DHT():capteur("humidite"){};
-    DHT(float T, float H):capteur("humidite"){
+    DHTCapt():capteur("humidite"){};
+    DHTCapt(float T, float H):capteur("humidite"){
       Temp=T;
       Hum=H;
+    }
+
+    void setHumidity(){
+      Hum=dht.readHumidity(); 
+    }
+
+    float getHumidity(){
+      return Hum; 
+    }
+
+    void setTemp(){
+      Temp=dht.readTemperature(); 
+    }
+
+    float getTemp(){
+      return Temp; 
     }
 };
 
@@ -64,32 +107,104 @@ class CaptCO2 : public capteur{
     }
 };
 
+   //-------------------------------
+   // INITIALISATION DES CLASSES AFFICHAGE 
+   //-------------------------------
+
+class Affichage {
+   protected : 
+    String nom; 
+  public :
+    Affichage();
+    Affichage(String n){
+      nom=n;
+    }
+};
+
+class LCD : public Affichage{
+  protected :
+    String Message;
+  public : 
+    LCD():Affichage("ecran"){};
+    LCD(String m):Affichage("ecran"){
+      Message=m;
+    }
+
+    void setTemp(float m){
+      lcd.setCursor(0, 1);
+      Message=(String)m;
+      lcd.print(m); 
+    }
+
+};
+
+class Servomoteur : public Affichage{
+  protected :
+    float angle;
+  public : 
+    Servomoteur():Affichage("ecran"){};
+    Servomoteur(float a):Affichage("ecran"){
+      angle=a;
+    }
+    void setAngle(float a){
+      angle=a;
+      myservo.write(a);
+    }
+};
 
 
+   //-------------------------------
+   // INITIALISATION OBJETS 
+   //-------------------------------
 
 BHP Pression;
+LCD Ecran;
+Servomoteur Servo1;
+Servomoteur Servo2;
+DHTCapt Humidity;
 
 
 
 void setup(){
   Serial.begin(9600);
+
+   //-------------------------------
+   // INITIALISATION CAPTEUR TEMP
+   //-------------------------------
   if (!bmp.begin()) {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
 
+   //-------------------------------
+   // INITIALISATION LCD
+   //-------------------------------
+   lcd.begin(16, 2);
+   lcd.setRGB(colorR, colorG, colorB);
+   lcd.print("Il fait : ");
+   lcd.setCursor(9, 1);
+   lcd.print("degre C");
+  
+   //-------------------------------
+   // INITIALISATION Servo
+   //-------------------------------
+   myservo.attach(servoPin);
+   myservo2.attach(servoPin2);
 
-  
-  
-  
-  
-  
+   dht.begin();
 }
 
 void loop(){
- 
+  angle=190;
   Pression.setPress();
-  Serial.println(Pression.getPress());
   Pression.setTemp();
-  Serial.println(Pression.getTemp());
+  Ecran.setTemp(Pression.getTemp());
+  Serial.println(Pression.getPress());
+  Servo1.setAngle(angle);
+
+  Humidity.setHumidity();
+  Humidity.setTemp();
+  Serial.println("Temperature = " + String(Humidity.getTemp())+" °C");
+  Serial.println("Humidite = " + String(Humidity.getHumidity())+" %");
+
 }
